9241123e2809502e485a38e75ba0c9b6b9a3dcc8
sabbna <146524499+sabbna@users.noreply.github.com>
2023-10-27 16:03:29 +0530

    Add files via upload

---
+++ color9.py
@@ -0,0 +1,2 @@
+color_list=["red","green","yellow","black"]
+print(color_list[0],color_list[-1])
---
+++ ordinal value11.py
@@ -0,0 +1,3 @@
+a=input("enter the word\n")
+for i in a:
+    print("ordinal value of",i,"is",ord(i),"\n")
